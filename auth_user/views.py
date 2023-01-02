fa6bd02db84715e654409b5659ed21baa1d23731
Andrew Buvria <buvrya228@gmail.com>
2023-01-02 15:02:50 +0200

    feat: add getting user by name

--- auth_user/views.py
+++ auth_user/views.py
@@ -20,8 +20,33 @@ from .serializers import (RoleVerifySerializer, UserReadSerializer,
 class UserEnvView(viewsets.ReadOnlyModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserReadSerializer
-    permission_classes = (IsAuthenticated, IsAllowedToGetObject)
+    permission_classes = (IsAuthenticated, ) #IsAllowedToGetObject
     authentication_classes = (JWTAuthentication,)
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+            'Expected view %s to be called with a URL keyword argument '
+            'named "%s". Fix your URL conf, or set the `.lookup_field` '
+            'attribute on the view correctly.' %
+            (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        lookup_value = self.kwargs[lookup_url_kwarg]
+
+        if lookup_value.isnumeric():
+            obj = queryset.get(id=lookup_value)
+        else:
+             obj = queryset.get(username=lookup_value)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
 
 class TokenObtainCustomView(TokenObtainPairView):
     serializer_class = TokenObtainPairSerializer
